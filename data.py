fd6169bfb17c19f8d03e545968a3468948af410c
Mark Chen <markchen555@gmail.com>
2018-01-24 18:48:16 -0800

    Doc: added articles data file

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+  articles = [
+    {
+      'id': 1,
+      'title': 'Article One',
+      'body': 'The mission of the Python Software Foundation is to promote, protect, and advance the Python programming language, and to support and facilitate the growth of a diverse and international community of Python programmers.',
+      'author': 'Brad Traversy',
+      'create_date': '1/24/2018'
+    },
+    {
+      'id': 2,
+      'title': 'Article Two',
+      'body': 'The mission of the Python Software Foundation is to promote, protect, and advance the Python programming language, and to support and facilitate the growth of a diverse and international community of Python programmers.',
+      'author': 'Brad Traversy',
+      'create_date': '1/24/2018'
+    },
+    {
+      'id': 3,
+      'title': 'Article Three',
+      'body': 'The mission of the Python Software Foundation is to promote, protect, and advance the Python programming language, and to support and facilitate the growth of a diverse and international community of Python programmers.',
+      'author': 'Brad Traversy',
+      'create_date': '1/24/2018'
+    }
+  ]
+  return articles
