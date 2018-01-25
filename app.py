8e400ae5da6837083f5447714c2adbddcc7f09d3
Mark Chen <markchen555@gmail.com>
2018-01-24 18:47:35 -0800

    Doc: implemented about, article and articles page

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template
+from data import Articles
+
 
 app = Flask(__name__)
 
+Articles = Articles()
 
+# home route
 @app.route('/')
 def index():
   return render_template('home.html')
+
+# about route
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+# articles route
+@app.route('/articles')
+def articles():
+  return render_template('articles.html', articles = Articles)
+
+# eatch article routes
+@app.route('/article/<string:id>/')
+def article(id):
+  return render_template('article.html', id = id)
 
 if __name__ == '__main__':
   app.run(debug=True)
